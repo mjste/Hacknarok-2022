c9c49d71e30120a5545968ce08f98bb9347f151c
Adam <adam.p.kulczycki@gmail.com>
2022-04-02 18:53:52 +0200

    DaysGrid added

---
+++ DaysGrid.tsx
@@ -0,0 +1,55 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+interface GridElementProps{
+  opacity: number
+}
+const GridElement: React.FC<GridElementProps> =({opacity}) => {
+  return(
+    <View style={[styles.square,{opacity}]}>
+    </View>
+  );
+}
+export const DaysGrid = () =>{
+  const numbers = [];
+  for (let i=0;i<40;i++){
+    numbers.push(i);
+  }
+  const listItems = numbers.map((number,i) =>  <GridElement opacity={0.03*i+0.1}/>);
+  return (
+    <View style={styles.container}>
+      {listItems}
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flexWrap: "wrap",
+    flexDirection: 'row',
+    height: '30%',
+    width: '100%',
+    backgroundColor: '#fff',
+    alignItems: 'center',
+  },
+  text:{
+    width: 'auto',
+    textAlign: 'center',
+  },
+  grid: {
+    backgroundColor: '#000',
+    alignItems: 'center'
+
+  },
+  square: {
+    justifyContent: 'center',
+    alignContent: 'center',
+    width: '9%',
+    height: '24%',
+    backgroundColor: "green",
+    margin:1
+  },
+});
