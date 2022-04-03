cbd5d7b9614e2fbd9cc1565792ea8e5d1c87081c
jchyczynski <jasiek.ch16@gmail.com>
2022-04-03 02:46:19 +0200

    added WeeklySummary

--- screens/Resolutions/Resolutions.tsx
+++ screens/Resolutions/Resolutions.tsx
@@ -1,35 +1,49 @@
-import { FlatList, SafeAreaView, StyleSheet, StatusBar, Text, View, ListRenderItem } from 'react-native'
-import { rowData, IRow, headerData, IHeader } from './data'
-import { Row } from './Row'
-import { Header } from './Header'
+import {
+  FlatList,
+  SafeAreaView,
+  StyleSheet,
+  StatusBar,
+  Text,
+  View,
+  ListRenderItem,
+} from "react-native";
+import { rowData, IRow, headerData, IHeader } from "./data";
+import { Row } from "./Row";
+import { Header } from "./Header";
+import { WeeklySummary } from "./WeeklySummary";
+import React from "react";
 interface renderProps {
-	row: IRow
+  row: IRow;
 }
 
 const Resolutions = () => {
-	const renderRow: ListRenderItem<IRow> = ({ item }) => <Row date={item.date} states={item.states} />
+  const renderRow: ListRenderItem<IRow> = ({ item }) => (
+    <Row date={item.date} states={item.states} />
+  );
 
-	return (
-		<SafeAreaView style={styles.container}>
-			<Header names={headerData.names}></Header>
-			<FlatList
-				inverted
-				data={[...rowData].reverse()}
-				renderItem={renderRow}
-				keyExtractor={row => row.id}
-				horizontal={false}
-				// initialScrollIndex={rowData.length - 1}
-			/>
-		</SafeAreaView>
-	)
-}
+  return (
+    <SafeAreaView style={styles.container}>
+      <Header names={headerData.names}></Header>
+
+      <FlatList
+        inverted
+        data={[...rowData].reverse()}
+        renderItem={renderRow}
+        keyExtractor={(row) => row.id}
+        horizontal={false}
+        // initialScrollIndex={rowData.length - 1}
+      />
+      <WeeklySummary />
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
-	container: {
-		backgroundColor: '#111111',
-		flex: 1,
-		marginTop: StatusBar.currentHeight || 0,
-	},
-})
+  container: {
+    backgroundColor: "#111111",
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+});
 
-export default Resolutions
+export default Resolutions;
