c6b97b6ddf07a931d216e2aad2d84b015f5ba13a
jchyczynski <jasiek.ch16@gmail.com>
2022-04-03 10:45:55 +0200

    feedMessages

---
+++ screens/Home/data.tsx
@@ -0,0 +1,8 @@
+export const feedMessages = [
+  "Missed her deadline for: make an appointment with JPII! Mock her! 🍅",
+  "Completed: adopt a racoon 🦝. That's how you do it!",
+  "Completed: save a hedgehog 🦔. Impressive!",
+  "Got 100% success rate on the latest Weekly Summary!",
+  "Lost 100zł by missing the deadline for: SysOpy with Nabagło!",
+  "Completed: send internship application 5 days before the deadline! GG EZ 🏆",
+];
