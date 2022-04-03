daffbf9f09db30587057f5c219c031b41dabf69c
Adam <adam.p.kulczycki@gmail.com>
2022-04-03 09:53:56 +0200

    profileTab prototype

--- profileTab.tsx
+++ profileTab.tsx
@@ -1,8 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
 import { OrgInfo } from './OrgInfo';
 
 const photo1 = require('./MockPhotos/mike.jpg')
+const photo3 = require('./MockPhotos/indeks.jpg')
+const photo2 = require('./MockPhotos/juliette.jpg')
+
+export const FeedList = () =>{
+  const numbers = [];
+  for (let i=0;i<8;i++){
+    numbers.push(i);
+  }
+  const listItems = numbers.map((number,i) =>  
+  <FeedElement key={i} name='No name lady' photo={photo3} TomatoesCounter={i} LikesCounter={4-i}  />);
+  listItems.push(<FeedElement name ='Juliette' photo={photo2} TomatoesCounter={21} LikesCounter={37}/>);
+  return (
+    <View style={styles.container}>
+      {listItems}
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+interface FeedElementProps{
+  name: string
+  photo: any
+  TomatoesCounter: number
+  LikesCounter: number
+}
+const FeedElement: React.FC<FeedElementProps> =({name, photo, TomatoesCounter, LikesCounter}) => {
+  return (
+      <View style={styles.feedElement}>
+          <Image style = {styles.photo_small} source={photo}/>
+          <View style={styles.text}>
+          <Text>{name}</Text>
+          </View>
+      </View>
+  );
+}
+
 
  interface ProfileInfoProps{
     name: string
@@ -10,15 +46,18 @@ const photo1 = require('./MockPhotos/mike.jpg')
   }
   export const ProfileInfo: React.FC<ProfileInfoProps> =({name, daysAmount}) => {
     return (
-        <View style={styles.feedElement}>
-            <Image style = {styles.photo} source={photo1}/>
-            <View style={styles.text}>
-                <Text style={styles.boldText}>{name}</Text>
-                <Text>You are with us for: <Text style={styles.boldText}>{daysAmount}</Text> days</Text>
-            </View>
+        <View style = {styles.profileInfo1}>
+          <View style={styles.profileInfo}>
+            <OrgInfo name={'Przytul jeża'} amountGiven={21.37}/>
             <View style={styles.orgInfo}>
-            <OrgInfo  name={'Przytul jeża'} amountGiven={21.37}/>
+              <Text style={styles.boldText}>Jan Paweł</Text>
+              <Image style ={styles.photo} source={photo1}/>
+              <Text>friens:</Text>
+              <ScrollView>
+                  <FeedList/>
+              </ScrollView>
             </View>
+          </View>
         </View>
     );
 }
@@ -26,32 +65,57 @@ const photo1 = require('./MockPhotos/mike.jpg')
 
 
 const styles = StyleSheet.create({
-  text:{
-    flexWrap: "wrap",
-    flex: 1
+  scrollScreen:{
+    width: '100%',
+    height: '50%'
   },
   boldText:{
     fontWeight: "bold",
-    color: "green"
+    color: "green",
   },
   orgInfo:{
-    margin: 5,
+    alignItems: 'center',
     alignSelf: 'flex-start'
   },
-  feedElement: {
-    marginTop: 20,
-    marginLeft: 20,
+  profileInfo: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+  profileInfo1: {
+    flex: 1,
+    flexDirection: 'column',
     width: '100%',
-    height: 100,
+    height: '100%',
     alignItems: 'stretch',
-    flex:1,
-    flexDirection: 'row'
   },
   photo: {
       alignContent: 'flex-end',
       width: 100,
       height: 100,
-      borderRadius: 500,
-      margin: 5
-  }
+      borderRadius: 100,
+      margin: 5,
+  },
+  photo_small: {
+    alignContent: 'flex-end',
+    width: 70,
+    height: 70,
+    borderRadius: 100,
+    margin: 5,
+},
+  text:{
+    flexGrow:1,
+
+  },
+  feedElement: {
+    width: '100%',
+    height: 70,
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  container: {  
+    height: '100%',
+    width: '100%',
+    backgroundColor: '#fff',
+    alignItems: 'flex-start',
+  },
 });
